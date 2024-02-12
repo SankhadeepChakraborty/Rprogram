faa7ff40904e0dd80c28569e864f5389081757c5
SANKHADEEP CHAKRABORTY <145654901+SankhadeepChakraborty@users.noreply.github.com>
2024-02-12 22:14:13 +0530

    Add files via upload

---
+++ vector.R
@@ -0,0 +1,18 @@
+# Data structure in R
+ #vectors , matrix , array , list , dataframe
+
+#elements of vector are known as components
+# length() : no. of elements in vector
+#atomic vector and list
+#vector is a sequence of vector and store the same data type
+ # how we create vector
+a1<-c(3,4,5,6)
+a1
+b1<- -3
+b1
+seq<-seq(1,5,by= .5)
+seq
+sq<-seq(1,3,length.out =10)  
+sq
+
+# numeric vector
